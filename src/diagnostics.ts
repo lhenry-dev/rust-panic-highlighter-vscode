a0c98d1632fe0a22a1acb65ce6e179bcd0261da5
lhenry-dev <lhenry.dev1@gmail.com>
2024-11-20 13:51:27 +0100

    feat: version publish 20/11

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,25 +1,25 @@
 import * as vscode from 'vscode';
 
 enum PanicType {
-    Unwrap = "unwrap(",
-    UnwrapUnchecked = "unwrap_unchecked(",
-    UnwrapErr = "unwrap_err(",
-    UnwrapErrUnchecked = "unwrap_err_unchecked(",
-    Expect = "expect(",
-    Panic = "panic!(",
-    Todo = "todo!(",
-    Unimplemented = "unimplemented!("
+    unwrap = "unwrap(",
+    unwrap_unchecked = "unwrap_unchecked(",
+    unwrap_err = "unwrap_err(",
+    unwrap_err_unchecked = "unwrap_err_unchecked(",
+    expect = "expect(",
+    panic = "panic!(",
+    todo = "todo!(",
+    unimplemented = "unimplemented!("
 }
 
 const DiagnosticMessages: Record<PanicType, string> = {
-    [PanicType.Unwrap]: "This line contains an 'unwrap()', which will panic if the result is None or Err.",
-    [PanicType.UnwrapUnchecked]: "This line contains an 'unwrap_unchecked()', which can cause undefined behavior if the result is None or Err.",
-    [PanicType.UnwrapErr]: "This line contains an 'unwrap_err()', which will panic if the result is Ok.",
-    [PanicType.UnwrapErrUnchecked]: "This line contains an 'unwrap_err_unchecked()', which can cause undefined behavior if the result is Ok.",
-    [PanicType.Expect]: "This line contains an 'expect()', which will panic if the result is None or Err.",
-    [PanicType.Panic]: "This line contains a 'panic!' which can cause a runtime panic.",
-    [PanicType.Todo]: "This line contains a 'todo!' macro, which is a placeholder and will panic if executed.",
-    [PanicType.Unimplemented]: "This line contains an 'unimplemented!' macro, which will panic if executed."
+    [PanicType.unwrap]: "This line contains an 'unwrap()', which will panic if the result is None or Err.",
+    [PanicType.unwrap_unchecked]: "This line contains an 'unwrap_unchecked()', which can cause undefined behavior if the result is None or Err.",
+    [PanicType.unwrap_err]: "This line contains an 'unwrap_err()', which will panic if the result is Ok.",
+    [PanicType.unwrap_err_unchecked]: "This line contains an 'unwrap_err_unchecked()', which can cause undefined behavior if the result is Ok.",
+    [PanicType.expect]: "This line contains an 'expect()', which will panic if the result is None or Err.",
+    [PanicType.panic]: "This line contains a 'panic!' which can cause a runtime panic.",
+    [PanicType.todo]: "This line contains a 'todo!' macro, which is a placeholder and will panic if executed.",
+    [PanicType.unimplemented]: "This line contains an 'unimplemented!' macro, which will panic if executed."
 };
 
 export function updateDiagnostics(doc: vscode.TextDocument, diagnosticCollection: vscode.DiagnosticCollection, decorationType: vscode.TextEditorDecorationType): void {
@@ -54,11 +54,6 @@ export function updateDiagnostics(doc: vscode.TextDocument, diagnosticCollection
             const diagnosticMessage = DiagnosticMessages[foundType];
             const startIndex = line.text.indexOf(foundType);
 
-            // Skip lines that are too short to contain a panic, BUG FIX
-            if (line.text.length <= 2) {
-                continue;
-            }
-
             const range = new vscode.Range(i, startIndex, i, line.text.length);
             rangesToDecorate.push(range);
 
@@ -75,7 +70,9 @@ export function updateDiagnostics(doc: vscode.TextDocument, diagnosticCollection
         editor.setDecorations(decorationType, rangesToDecorate);
     }
 
-    if (diagnostics.length !== 0) {
+    if (diagnostics.length === 0) {
+        diagnosticCollection.delete(doc.uri);
+    } else {
         diagnosticCollection.set(doc.uri, diagnostics);
     }
 }
