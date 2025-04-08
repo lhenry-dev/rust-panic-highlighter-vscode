e5e4425e4d079e1b59101e9ff49d3edb22074af0
Lucas H <lumane35@gmail.com>
2025-04-09 00:16:08 +0200

    fix: release script

--- release.sh
+++ release.sh
@@ -26,7 +26,7 @@ fi
 
 # 1. Bump the version in package.json based on the passed parameter
 echo "Bumping version ($1)..."
-npm version $1 -m "Release version %s"
+npm version $1 --no-git-tag-version -m "Release version %s"
 
 VERSION=$(node -p "require('./package.json').version")
 PACKAGE_NAME=$(node -p "require('./package.json').name")
@@ -37,33 +37,32 @@ mkdir -p releases
 
 # 3. Create the .vsix file and move it to the /releases folder
 echo "Creating .vsix file..."
-vsce package
+vsce package "$VERSION" --no-git-tag-version
 mv "$PACKAGE_NAME-$VERSION.vsix" releases/
 
 # 4. Publish the extension with VSCE
 echo "Publishing the extension..."
 echo "Version: $VERSION"
-vsce publish "$VERSION"
+vsce publish "$VERSION" --no-git-tag-version
 
 # 5. Stage the .vsix file for commit
 echo "Adding the .vsix file to Git..."
-git add releases/"$PACKAGE_NAME-*.vsix"
+git add .
 
 # 6. Commit the changes (including the .vsix file)
 echo "Committing changes..."
 git commit -m "chore: Add $PACKAGE_NAME-$VERSION .vsix file"
 
-# 7. Push the commit first
-echo "Pushing the .vsix file commit..."
+# 7. Push the commit
+echo "Pushing changes..."
 git push origin main
 
 # 8. Create a git tag for the new version
 echo "Creating tag v$VERSION..."
 git tag -a "v$VERSION" -m "chore: Release $PACKAGE_NAME version $VERSION"
 
-# 9. Push the commit and the tag
-echo "Pushing changes..."
-git push origin main
+# 9. Push the the tag
+echo "Pushing the tag..."
 git push origin "v$VERSION"
 
 echo "Release complete!"
